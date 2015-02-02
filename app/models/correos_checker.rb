862f6eabdd628ad6bf4e355850e905617577b863
Jose Antonio Lareo Domínguez <lareomerlin@gmail.com>
2015-02-02 16:42:25 +0000

    Add error control (cannot open correos webpage and html layout has changed)

--- app/models/correos_checker.rb
+++ app/models/correos_checker.rb
@@ -14,28 +14,36 @@ class CorreosChecker < ActiveRecord::Base
   def check_tracking_state
 
     url = "http://aplicacionesweb.correos.es/localizadorenvios/track.asp?numero=#{tracking_number}"
-    doc = Nokogiri::HTML(open(url))
 
-    #debug#
-    #tempStatus = "#{Time.now.to_s}#{Time.now.to_s}"
-    #tempStatus = "No disponemos de infor"
-    #tempStatus = "Entregado"
-    #doc = Nokogiri::HTML("<html><body><div><tr class='txtCabeceraTabla'><td>#{tempStatus}</td></tr></div></body></html>")
-    #######
+    begin
+      doc = Nokogiri.HTML(open(url))
 
+      #debug#
+      #tempStatus = "#{Time.now.to_s}#{Time.now.to_s}"
+      #tempStatus = "No disponemos de infor"
+      #tempStatus = "Entregado"
+      #doc = Nokogiri::HTML("<html><body><div><tr class='txtCabeceraTabla'><td>#{tempStatus}</td></tr></div></body></html>")
+      #######
 
-    if doc.css('body div').first.to_s.match(/No disponemos de infor/)
-      remove_tracking_number if created_at < 5.days.ago
-    else
-      current_status = doc.css('.txtCabeceraTabla').last.css('td').last.text.strip
-
-      if current_status != status
-        updated_attributes = { status: current_status }
-        updated_attributes[:completed_at] = Time.now if current_status.include?('Entregado')
-        self.class.where(id: id).update_all(updated_attributes)
-        self.reload
-        UserMailer.status_updated(self).deliver
+      if doc.css('body div').first.to_s.match(/No disponemos de infor/)
+        remove_tracking_number if created_at < 5.days.ago
+      else
+        current_status = doc.css('span.txtNormal').last
+        if current_status.blank?
+          puts "html layout has changed?"
+        else
+          current_status = current_status.text.strip
+          if current_status != status
+            updated_attributes = { status: current_status }
+            updated_attributes[:completed_at] = Time.now if current_status.include?('Entregado')
+            self.class.where(id: id).update_all(updated_attributes)
+            self.reload
+            UserMailer.status_updated(self).deliver
+          end
+        end
       end
+    rescue Exception => e
+      puts "Couldn't read \"#{ url }\": #{ e }"
     end
   end
 
