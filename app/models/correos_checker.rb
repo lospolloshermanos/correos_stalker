61197678b935bea197a68970d097908ab0b70154
Jose Antonio Lareo Domínguez <lareomerlin@gmail.com>
2015-02-05 17:38:29 +0000

    Fix typo

--- app/models/correos_checker.rb
+++ app/models/correos_checker.rb
@@ -3,7 +3,7 @@ class CorreosChecker < ActiveRecord::Base
   before_save :downcase_email
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_TRACKING_NUMBER = /\A[\w+]+\z/i
+  VALID_TRACKING_NUMBER = /\A[\w]+\z/i
 
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
