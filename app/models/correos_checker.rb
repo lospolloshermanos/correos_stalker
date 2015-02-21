a8a44396e3450db419ac6f31ed433d13da14a4a1
Jose Antonio Lareo Domínguez <lareomerlin@gmail.com>
2015-02-21 15:42:00 +0000

    Allow to re-add a tracking number from the email

--- app/models/correos_checker.rb
+++ app/models/correos_checker.rb
@@ -13,9 +13,9 @@ class CorreosChecker < ActiveRecord::Base
 
   validates :description, length: { maximum: 255 }
 
-  scope :unconfirmed, -> { where("confirmed is FALSE and created_at < '#{1.days.ago}'") }
+  scope :unconfirmed, -> { where("confirmed is FALSE and updated_at < '#{1.days.ago}'") }
   scope :unfinished, -> { where('completed_at is NULL and confirmed is TRUE') }
-  scope :completed, -> { where("completed_at is not NULL and completed_at < '#{2.days.ago}'") }
+  scope :completed, -> { where("completed_at < '#{2.days.ago}'") }
 
   def check_tracking_state
 
@@ -25,8 +25,8 @@ class CorreosChecker < ActiveRecord::Base
       doc = Nokogiri.HTML(open(url))
 
       if doc.css('body div').first.to_s.match(/No disponemos de infor/)
-        if created_at < 7.days.ago
-          remove_tracking_number
+        if updated_at < 7.days.ago
+          unconfirm_tracking_number
           UserMailer.invalid_tracking_number(self).deliver
         end
       else
@@ -38,12 +38,11 @@ class CorreosChecker < ActiveRecord::Base
           if current_status != status
             updated_attributes = { status: current_status }
             updated_attributes[:completed_at] = Time.now if current_status.include?('Entregado')
-            self.class.update_all(updated_attributes)
-            self.reload
+            self.update_attributes(updated_attributes)
             UserMailer.status_updated(self).deliver
           else
-            if created_at < 60.days.ago
-              remove_tracking_number
+            if updated_at < 30.days.ago
+              unconfirm_tracking_number
               UserMailer.complete_time_exceeded(self).deliver
             end
           end
@@ -52,6 +51,10 @@ class CorreosChecker < ActiveRecord::Base
     rescue Exception => e
       puts "Couldn't read \"#{ url }\": #{ e }"
     end
+  end
+
+  def unconfirm_tracking_number
+    self.update_attribute(:confirmed, false)
   end
 
   def remove_tracking_number
