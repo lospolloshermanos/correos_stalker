d9f62ac5e023beeae3cc0344a5f53d912dff6d98
jlareo <lareomerlin@gmail.com>
2016-03-04 12:32:10 +0000

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => ENV['TRANSACTIONAL_EMAIL_DOMAIN'] }
 
   config.action_mailer.smtp_settings = {
-    :address   => ENV['TRANSACTIONAL_EMAIL_ADRESS'],
+    :address   => ENV['TRANSACTIONAL_EMAIL_ADDRESS'],
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => ENV['TRANSACTIONAL_EMAIL_USERNAME'],
